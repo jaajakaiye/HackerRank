85ef738e1b157d509ed5fa5f0b6bd0b78a506dae
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-04-29 21:54:27 -0400

    Update Solution.sql
    Added question to the answer.

--- SQL/Basic Select/Select All/Solution.sql
+++ SQL/Basic Select/Select All/Solution.sql
@@ -1,2 +1,15 @@
+#Query all columns (attributes) for every row in the CITY table.
+
+#The CITY table is described as follows:
+
+#City (table)
+#Field Type
+#ID    Number
+#Name   VARCHAR2(17)
+#CountryCode VARCHAR2(3)
+#DISTRICT VARCHAR2(20)
+#Population NUMBER
+
+
 SELECT *
 FROM CITY;
