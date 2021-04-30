0b79577c337fee795fce7787717358ab11225d3c
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-04-29 21:47:13 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Basic Select/Revising the Select Query II/Solution.sql
+++ SQL/Basic Select/Revising the Select Query II/Solution.sql
@@ -1,4 +1,17 @@
+#Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+#The CITY table is described as follows:
+#City (table)
+#Field Type
+#ID    Number
+#Name   VARCHAR2(17)
+#CountryCode VARCHAR2(3)
+#DISTRICT VARCHAR2(20)
+#Population NUMBER
+
 SELECT NAME
 FROM CITY
 WHERE COUNTRYCODE = 'USA'
   AND POPULATION > 120000;
+  
+  
