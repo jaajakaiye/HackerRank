1ceeea84106c5c37c31ad08ce9a7bf6877b2e6a7
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-05-04 09:43:22 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Basic Select/Weather Observation Station 1/Solution.sql
+++ SQL/Basic Select/Weather Observation Station 1/Solution.sql
@@ -1,2 +1,16 @@
+#Query a list of CITY and STATE from the STATION table.
+#The STATION table is described as follows:
+
+
+#Station (table)
+#Field Type
+#ID    Number
+#City   VARCHAR2(21)
+#State VARCHAR2(2)
+#LAT_N NUMBER
+#LONG_W NUMBER
+
+
+
 SELECT CITY, STATE
 FROM STATION;
