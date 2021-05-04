32a52d1b2ce01d09161c48aee26d08972750ef9e
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-05-04 10:47:15 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Basic Select/Higher Than 75 Marks/Solution.sql
+++ SQL/Basic Select/Higher Than 75 Marks/Solution.sql
@@ -1,3 +1,33 @@
+#Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. 
+#If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+#Input Format
+
+#Column Type
+#ID    Integer
+#Name  String
+#Marks Integer
+
+#The STUDENTS table is described as follows: 
+#The Name column only contains uppercase (A-Z) and lowercase (a-z) letters.
+
+#Sample Input
+#ID Name Marks
+#1  Ashely 81
+#2 Samantha 75
+#3 Julia    76
+#4 Belvet   84
+
+#Sample Output
+#Ashley
+#Julia
+#Belvet
+
+#Explanation
+
+Only Ashley, Julia, and Belvet have Marks > . If you look at the last three characters of each of their names, 
+#there are no duplicates and 'ley' < 'lia' < 'vet'.
+
 SELECT NAME
 FROM STUDENTS
 WHERE MARKS > 75
