f77a09f584ecd3fcbd988bd0aeea3a651ac4e134
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-05-04 15:03:12 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Alternative Queries/Draw The Triangle 1/Solution.sql
+++ SQL/Alternative Queries/Draw The Triangle 1/Solution.sql
@@ -1,2 +1,12 @@
+#P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+
+#Write a query to print the pattern P(20).
+
 SELECT RPAD('*', (21-LEVEL)*2, ' *')
 FROM DUAL CONNECT BY LEVEL <= 20;
