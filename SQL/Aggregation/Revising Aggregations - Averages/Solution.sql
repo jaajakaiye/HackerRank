7d8e989912fdb215b195573354e1b6985de124af
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-05-04 14:11:36 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Aggregation/Revising Aggregations - Averages/Solution.sql
+++ SQL/Aggregation/Revising Aggregations - Averages/Solution.sql
@@ -1,3 +1,19 @@
+#Query the average population of all cities in CITY where District is California.
+
+#Input Format
+
+#The City table is described as follows:
+
+#City
+#Field       Type
+#ID          NUMBER
+#NAME        VARCHAR2(17)
+#COUNTRYCODE VARCHAR2(3)
+#DISTRICT    VARCHAR2(20)
+#POPULATION  NUMBER
+
+
+
 SELECT AVG(POPULATION)
 FROM CITY
 WHERE DISTRICT = 'California';
