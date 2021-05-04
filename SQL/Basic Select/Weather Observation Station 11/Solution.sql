f708a5c8f4fa11c6c3ef7a99374a87664dc6f989
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-05-04 10:37:18 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Basic Select/Weather Observation Station 11/Solution.sql
+++ SQL/Basic Select/Weather Observation Station 11/Solution.sql
@@ -1,3 +1,21 @@
+#Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+#Input Format
+
+#The STATION table is described as follows:
+
+
+#Station (table)
+#Field Type
+#ID    Number
+#City   VARCHAR2(21)
+#State VARCHAR2(2)
+#LAT_N NUMBER
+#LONG_W NUMBER
+
+#where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
 SELECT DISTINCT CITY
 FROM STATION
 WHERE REGEXP_LIKE(UPPER(CITY), '^[^AEIOU]')
