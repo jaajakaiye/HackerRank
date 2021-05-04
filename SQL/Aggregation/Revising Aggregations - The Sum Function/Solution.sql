8d930d5d15541746e26b958ad6076215a357a0dd
Jumoke Ajakaiye <76570061+jaajakaiye@users.noreply.github.com>
2021-05-04 14:09:23 -0400

    Update Solution.sql
    Added question to answer.

--- SQL/Aggregation/Revising Aggregations - The Sum Function/Solution.sql
+++ SQL/Aggregation/Revising Aggregations - The Sum Function/Solution.sql
@@ -1,3 +1,17 @@
+#Query the total population of all cities in CITY where District is California.
+
+#Input Format
+
+#The City table is described as follows:
+
+#City
+#Field       Type
+#ID          NUMBER
+#NAME        VARCHAR2(17)
+#COUNTRYCODE VARCHAR2(3)
+#DISTRICT    VARCHAR2(20)
+#POPULATION  NUMBER
+
 SELECT SUM(POPULATION)
 FROM CITY
 WHERE DISTRICT = 'California';
